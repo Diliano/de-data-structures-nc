c4119b39a0e0e1eee0357587ef288f84c2a23a80
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-17 10:29:21 +0100

    create the Stack class, with passing tests for instantiating a quantity and storage property

--- src/my_stack.py
+++ src/my_stack.py
@@ -1,1 +1,4 @@
-# Implement your stack here
+class Stack():
+    def __init__(self):
+        self.quantity = 0
+        self.storage = {}
--- test/test_my_stack.py
+++ test/test_my_stack.py
@@ -1,1 +1,13 @@
-# Test your implementation here
+from src.my_stack import Stack
+
+def test_creates_instance_of_class():
+    test_stack = Stack()
+    assert isinstance(test_stack, Stack)
+
+def test_instance_is_instantiated_with_quantity_property_0():
+    test_stack = Stack()
+    assert test_stack.quantity == 0
+
+def test_instance_is_instantiated_with_empty_dict_storage_property():
+    test_stack = Stack()
+    assert test_stack.storage == {}
