8bdefa6bdcbcdb678d8dcfdfa550fe59b4667e96
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-17 19:57:17 +0100

    add functionality and passing tests for add() method

--- src/my_set.py
+++ src/my_set.py
@@ -1,3 +1,8 @@
 class Set():
     def __init__(self):
         self._elements = []
+
+    def add(self, element):
+        if element in self._elements:
+            return "No duplicates allowed"
+        self._elements.append(element)
--- test/test_my_set.py
+++ test/test_my_set.py
@@ -8,3 +8,20 @@ class TestInstantiation():
     def test_creates_empty_set_given_empty_input(self):
         test_set = Set()
         assert test_set._elements == []
+
+class TestAddMethod():
+    def test_add_method_adds_an_item_to_the_set(self):
+        # Arrange
+        test_set = Set()
+        # Act
+        test_set.add("apple")   
+        # Assert
+        assert test_set._elements == ["apple"]
+
+    def test_add_method_does_not_add_a_duplicate_to_the_set(self):
+        # Arrange
+        test_set = Set()
+        # Act
+        test_set.add("apple")   
+        # Assert
+        assert test_set.add("apple") == "No duplicates allowed"
